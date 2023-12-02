b9cac516f3641c3a00ce9b294f7c90ca8523b28a
Jakub Przybylinski <jak.przybylinski@gmail.com>
2023-12-03 00:33:35 +0100

    added solution

---
+++ day2/task1.py
@@ -0,0 +1,45 @@
+file = open('./day2/data.txt','r')
+f = file.readlines()
+file.close()
+#data
+red, green, blue = 12, 13, 14
+
+def is_valid(number: int, color: str) -> bool:
+    '''checks if number of specific cubes is allowed'''
+    match color:
+        case 'red':
+            if number > red:
+                return False
+            return True
+        case 'green':
+            if number > green:
+                return False
+            return True
+        case 'blue':
+            if number > blue:
+                return False
+            return True
+
+def draws(draws: list[str]) -> bool:
+    for draw in draws:
+        cubes = draw.split(',')
+        for cube in cubes:
+            cube = cube.strip()
+            cube = cube.split(' ')
+            quantity = int(cube[0]) #get number of cubes
+            color = cube[1] #get color of cubes
+            if not is_valid(quantity, color):
+                return False
+    return True
+
+sum = 0
+for i in f:
+    i = i.strip().replace(':',';')[5:].split(';')
+    id = i[0]
+    i = i[1:] #remove id from data
+    if draws(i):
+        sum += int(id)
+
+file = open('./day2/result1.txt','w')
+file.write(f'Answer: {sum}')
+file.close()
