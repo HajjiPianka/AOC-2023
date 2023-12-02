f99c9651890d013e8e7bb547bdd9dd0408ea65cb
Jakub Przybylinski <jak.przybylinski@gmail.com>
2023-12-03 00:49:33 +0100

    final solution and removal of old code

--- day2/task2.py
+++ day2/task2.py
@@ -1,24 +1,10 @@
 file = open('./day2/data.txt','r')
 f = file.readlines()
 file.close()
-"""
-def is_valid(number: int, color: str) -> bool:
-    '''checks if number of specific cubes is allowed'''
-    match color:
-        case 'red':
-            if number > red:
-                return False
-            return True
-        case 'green':
-            if number > green:
-                return False
-            return True
-        case 'blue':
-            if number > blue:
-                return False
-            return True
-"""
-def draws(draws: list[str]) -> bool:
+
+def min_color(draws: list[str]) -> bool:
+    #data 
+    red, green, blue = 0, 0, 0
     for draw in draws:
         cubes = draw.split(',')
         for cube in cubes:
@@ -26,17 +12,24 @@ def draws(draws: list[str]) -> bool:
             cube = cube.split(' ')
             quantity = int(cube[0]) #get number of cubes
             color = cube[1] #get color of cubes
-            # if not is_valid(quantity, color):
-            #     return False
-    # return True
+            match color:
+                case 'red':
+                    red = red if red > quantity else quantity
+                case 'green':
+                    green = green if green > quantity else quantity
+                case 'blue':
+                    blue = blue if blue > quantity else quantity
+    return [red, green, blue]
 
 sum = 0
 for i in f:
     i = i.strip().replace(':',';')[5:].split(';')
     id = i[0]
     i = i[1:] #remove id from data
-    if draws(i):
-        sum += int(id)
+    min_values = min_color(i)
+    curr = min_values[0] * min_values[1] * min_values[2]
+    sum += curr
+    curr = 0
 
 file = open('./day2/result2.txt','w')
 file.write(f'Answer: {sum}')
