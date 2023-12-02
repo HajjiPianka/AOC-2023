d64ab1eb44bc4369e33133db0e3e25d5eb2be46e
Jakub Przybylinski <jak.przybylinski@gmail.com>
2023-12-02 03:07:03 +0100

    replace strings

--- day1/task2.py
+++ day1/task2.py
@@ -1,10 +1,16 @@
-file = open('data.txt','r')
+file = open('./day1/data.txt','r')
 f = file.readlines()
 file.close()
 sum = 0
 def digit(string: str, reverse: bool = False):
     '''Returns first digit of a string, parsed with second argument returns last digit'''
     string = string.strip()
+    ###part 2
+    # replac estings with digits
+    strings =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    for i in range(len(strings)):
+        string = string.replace(strings[i],str(i+1))
+    ###
     if reverse:
         string = string[::-1]
     for i in string:
@@ -22,6 +28,6 @@ for i in f:
     num2 = digit(i,True)
     number = merge_ints(num1,num2)
     sum += number
-file = open('result.txt','w')
+file = open('result2.txt','w')
 file.write(f'Answer: {sum}')
 file.close()
